f5aaa5bc532099db59ba3c9afe966b3e0ecc32ff
Andy <andreapauline.deguzman@gmail.com>
2021-06-21 23:52:16 +0800

    expense-tracker: adding filter and day 3 lecture of coding along

--- react-challenges/expense-tracker/src/App.js
+++ react-challenges/expense-tracker/src/App.js
@@ -1,34 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Expenses from "./Expenses/Expenses";
+import NewExpense from "./NewExpense/NewExpense";
 
 const DUMMY_EXPENSES = [
   {
     title: "Car Insurance",
-    amount: "100,000,000",
-    date: new Date(),
-  },
-  {
-    title: "Car Insurance",
-    amount: "294.67",
+    amount: "1,000,000",
     date: new Date(2021, 2, 28),
   },
   {
-    title: "Car Insurance",
-    amount: "294.67",
-    date: new Date(2019, 2, 28),
+    title: "Brand New Laptop",
+    amount: "200,000",
+    date: new Date(2020, 2, 28),
   },
   {
-    title: "Car Insurance",
-    amount: "294.67",
-    date: new Date(2022, 11, 28),
+    title: "House and Lot",
+    amount: "20,000,00",
+    date: new Date(2019, 8, 6),
+  },
+  {
+    title: "Insurance",
+    amount: "30,000",
+    date: new Date(2018, 12, 7),
   },
 ];
 
 const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
   return (
     <div className="App">
-      <Expenses expenses={DUMMY_EXPENSES} />
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses expenses={expenses} />
     </div>
   );
 };
--- react-challenges/expense-tracker/src/Expenses/Expenses.js
+++ react-challenges/expense-tracker/src/Expenses/Expenses.js
@@ -1,12 +1,28 @@
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesList from "./ExpensesList.js";
+import { useState } from "react";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setSelectedYear] = useState("2021");
+  console.log(filteredYear);
+  const filterChangedHandler = (selectedValue) => {
+    setSelectedYear(selectedValue);
+  };
+
+  const filteredExpenses = props.expenses.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+  console.log(props);
   return (
     <div>
       <Card className="expenses">
-        <ExpensesList items={props.expenses} />
+        <ExpensesFilter
+          onChangeFilter={filterChangedHandler}
+          selectedValue={filteredYear}
+        />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
